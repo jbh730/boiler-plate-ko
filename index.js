fa02ea75b322245b613e240be6f67bbde6977e1f
정병현 <jeongbyeonghyeon@jeongbyeonghyeon-ui-MacBookPro.local>
2021-05-11 10:54:23 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jbh730:qudgus@88@cluster0.bugja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}). then(() => console.log('MongoDB Conneted...'))
+  . catch(err => console.log(err))
+
+// app.get('/', (req, res) => res.setEncoding('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
